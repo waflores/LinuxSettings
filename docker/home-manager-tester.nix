7cb152d9d5221f87d30f1b842bcb5958f0e61688
Will Flores <waflores956@gmail.com>
2025-03-16 02:58:23 +0000

    WIP: a working nix container 2 layer image but need to troubleshoot the nix store ownership.

--- docker/home-manager-tester.nix
+++ docker/home-manager-tester.nix
@@ -1,35 +1,32 @@
 # home-manager-tetser.nix - environment to test out our changes to our dotfiles
 { pkgs ? import <nixpkgs> { config = { }; overlays = [ ]; }
 }:
-pkgs.dockerTools.buildImageWithNixDb
-{
+pkgs.dockerTools.buildLayeredImageWithNixDb {
   name = "home-manager-testenv";
   # tag = "20250315";
-  # uid = 1000;
-  # gid = 1000;
-  copyToRoot = pkgs.buildEnv {
-    name = "home-manager-root";
-    paths = with pkgs; [
-      coreutils
-      bash
-      dockerTools.binSh
-      jq
-      nixVersions.latest
-      (fakeNss.override
-        {
-          extraPasswdLines = [ "will:x:1000:1000:Will:/home/will:/bin/bash" ];
-          extraGroupLines = [ "will:x:1000:" ];
+  uid = 1000;
+  gid = 1000;
+  uname = "will";
+  gname = "will";
+  contents = with pkgs; [
+    coreutils
+    bash
+    dockerTools.binSh
+    jq
+    nixVersions.latest
+    (fakeNss.override
+      {
+        extraPasswdLines = [ "will:x:1000:1000:Will:/home/will:/bin/bash" ];
+        extraGroupLines = [ "will:x:1000:" ];
 
-        })
-      nix-output-monitor
-
-    ];
-    pathsToLink = [ "/bin" "/etc" "/var" "/home" ];
-  };
-
+      })
+    nix-output-monitor
+  ];
   config = {
     # Cmd = [ "${pkgs.hello}/bin/hello" ];
-    # WorkingDir = "/data";
+    WorkingDir = "/home/will";
+    User = "will";
     # Volumes = { "/data" = { }; }
   };
+  maxLayers = 2;
 }
