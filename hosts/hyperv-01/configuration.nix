44b3defeae4b0ee59cb5017655729b0860ce539f
Will Flores <waflores956@gmail.com>
2025-03-22 21:55:40 +0000

    Created a user for hyperv-01; also corrected a typo home-shared.nix to host-shared.nix

--- hosts/hyperv-01/configuration.nix
+++ hosts/hyperv-01/configuration.nix
@@ -4,13 +4,16 @@
   ...
 }:
 {
-  imports = [ inputs.home-manager.nixosModules.default ];
+  imports = [ inputs.self.nixosModules.host-shared ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
+
+  # on nixos this either isNormalUser or isSystemUser is required to create the user.
+  users.users.will.isNormalUser = true;
 
   # for testing purposes only, remove on bootable hosts.
   boot.loader.grub.enable = pkgs.lib.mkDefault false;
   fileSystems."/".device = pkgs.lib.mkDefault "/dev/null";
 
-  system.stateVersion = "24.11"; # initial nixos state
+  system.stateVersion = pkgs.lib.versions.majorMinor pkgs.lib.version; # initial nixos state
 }
