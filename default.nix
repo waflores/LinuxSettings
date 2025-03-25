6314467d6cd4ac840dd2091faff46e9e5952359f
Will Flores <waflores956@gmail.com>
2025-03-25 23:52:45 +0000

    XXX: stubbing out a default.nix.

---
+++ default.nix
@@ -0,0 +1,25 @@
+# This file provides backward compatibility to nix < 2.4 clients
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+
+  inherit (lock.nodes.flake-compat.locked)
+    owner
+    repo
+    rev
+    narHash
+    ;
+
+  flake-compat = fetchTarball {
+    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+    sha256 = narHash;
+  };
+
+  flake = import flake-compat { src = ./.; };
+
+  warn = msg: builtins.trace "[1;31mwarning: ${msg}[0m";
+  example = ''
+    nixpkgs.overlays=[(import "''${builtins.fetchurl "https://github.com/nix-community/nixpkgs-wayland/archive/master.tar.gz"}/overlay.nix")];
+  '';
+  warning = "change your 'nixpkgs-wayland' import statement to import 'overlay.nix' directly. example:\n  ${example}";
+in
+warn warning flake.defaultNix
