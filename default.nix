af42c13790ccf88c68fdbb9ce366cc1e8e377eef
Will Flores <waflores956@gmail.com>
2025-03-26 00:46:38 +0000

    Trying to make shell.nix and default.nix repeatable/hermetic.

--- default.nix
+++ default.nix
@@ -2,24 +2,24 @@
 let
   lock = builtins.fromJSON (builtins.readFile ./flake.lock);
 
-  inherit (lock.nodes.flake-compat.locked)
+  inherit (lock.nodes.nixpkgs.locked)
     owner
     repo
     rev
     narHash
     ;
 
-  flake-compat = fetchTarball {
+  nixpkgs = fetchTarball {
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
     sha256 = narHash;
   };
 
-  flake = import flake-compat { src = ./.; };
-
-  warn = msg: builtins.trace "[1;31mwarning: ${msg}[0m";
-  example = ''
-    nixpkgs.overlays=[(import "''${builtins.fetchurl "https://github.com/nix-community/nixpkgs-wayland/archive/master.tar.gz"}/overlay.nix")];
-  '';
-  warning = "change your 'nixpkgs-wayland' import statement to import 'overlay.nix' directly. example:\n  ${example}";
+  pkgs = import nixpkgs { };
 in
-warn warning flake.defaultNix
+ pkgs.mkShellNoCC {
+   packages = with pkgs; [
+     cowsay
+   ];
+
+  GREETING = "Hello, from default.nix!";
+ }
--- shell.nix
+++ shell.nix
@@ -1,5 +1,21 @@
+# This file provides backward compatibility to nix < 2.4 clients
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+
+  inherit (lock.nodes.nixpkgs.locked)
+    owner
+    repo
+    rev
+    narHash
+    ;
+
+  nixpkgs = fetchTarball {
+    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+    sha256 = narHash;
+  };
+in
 {
-  pkgs ? import <nixpkgs> { },
+  pkgs ? import nixpkgs { },
 }:
 
 import ./devshells/default.nix { inherit pkgs; }
