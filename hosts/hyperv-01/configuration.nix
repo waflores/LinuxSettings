c1b2c87ece7cabce0327861c2b86d7dd07fa6c4d
Will Flores <waflores956@gmail.com>
2025-03-30 18:32:19 +0000

    Adding nixConfig.extra-trusted-substituters and keys.

--- hosts/hyperv-01/configuration.nix
+++ hosts/hyperv-01/configuration.nix
@@ -5,14 +5,13 @@
 }:
 {
   imports = [ inputs.self.nixosModules.host-shared ];
-  # keep-sorted start
 
   # for testing purposes only, remove on bootable hosts.
   boot.loader.grub.enable = pkgs.lib.mkDefault false;
   fileSystems."/".device = pkgs.lib.mkDefault "/dev/null";
   nixpkgs.hostPlatform = "x86_64-linux";
   system.stateVersion = pkgs.lib.versions.majorMinor pkgs.lib.version; # initial nixos state
+
   # on nixos this either isNormalUser or isSystemUser is required to create the user.
   users.users.will.isNormalUser = true;
-  # keep-sorted end
 }
