ace4a9d90896fbc59751aba2f04c7cfe4ffb03ed
Will Flores <waflores956@gmail.com>
2025-04-05 18:41:36 +0000

    Enabling an overriden version of git-lfs.

--- hosts/hyperv-01/users/will/home-configuration.nix
+++ hosts/hyperv-01/users/will/home-configuration.nix
@@ -1,8 +1,14 @@
-{ flake, ... }:
+{ flake, perSystem, ... }:
 {
 
   imports = [ flake.homeModules.host-shared ];
+
+  # NOTE: We can override our git configuration here
   programs.git = {
     userEmail = "waflores956+hyperv-01@gmail.com";
+    lfs = {
+      enable = true;
+      package = perSystem.nixpkgs-git-lfs.git-lfs;
+    };
   };
 }
