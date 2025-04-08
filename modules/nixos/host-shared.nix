bb2cee6d5ff388d9d155942f9467b924c7d89024
Will Flores <waflores956@gmail.com>
2025-04-08 00:34:08 +0000

    XXX: refactoring security.tpm2 config.

--- modules/nixos/host-shared.nix
+++ modules/nixos/host-shared.nix
@@ -36,9 +36,13 @@
     };
   };
 
-  security.tpm2.enable = true;
-  security.tpm2.pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
-  security.tpm2.tctiEnvironment.enable = true; # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
+
+  security.tpm2 = {
+    enable = true;
+    pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+    tctiEnvironment.enable = true; # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
+  };
+
 
   ## Localization defaults
   # Set our timezone
