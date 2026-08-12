227c94ea92f66d3976b15c5566fe0c90b81f2b56
Will Flores <waflores956@gmail.com>
2026-08-11 21:11:00 -0400

    Adding a new beaglebone configuration. Updating flake.lock

--- flake.nix
+++ flake.nix
@@ -16,20 +16,20 @@
     # home.stateVersion is set in: modules/home/host-shared.nix
     # system.stateVersion is set in:  hosts/*/configuration.nix
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "https://github.com/nix-community/home-manager/archive/36662afed2fa1c9b69bdd03edb92ad572202ca20.tar.gz"; # master - 2026-07-28
+    home-manager.url = "https://github.com/nix-community/home-manager/archive/f404edbfa4117810c96b97048299242fc50e5362.tar.gz"; # master - 2026-08-11
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    nix-index-database.url = "https://github.com/Mic92/nix-index-database/archive/11665045df8b9938ef811a3bfdc65cffb02b4b70.tar.gz";
+    nix-index-database.url = "https://github.com/Mic92/nix-index-database/archive/14d55b8069119e3b88da7aa2f6c97f86a2cd3cd6.tar.gz";
     # Framework to remotely deploy new configurations
     nixos-anywhere.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-anywhere.url = "https://github.com/nix-community/nixos-anywhere/archive/91fc9b70fc295258c366cce8627efb6f185fd9fb.tar.gz";
-    nixos-hardware.url = "https://github.com/NixOS/nixos-hardware/archive/2e790b0a6be8ec2b76174ac0931b8ff11919ec98.tar.gz";
+    nixos-anywhere.url = "https://github.com/nix-community/nixos-anywhere/archive/036bd2423203b1432f36621404289832183cfecd.tar.gz";
+    nixos-hardware.url = "https://github.com/NixOS/nixos-hardware/archive/6ed13b1d888d5cb07dbb0723eb1df86bbacd0b9c.tar.gz";
     nixpkgs-git-lfs.url = "https://github.com/NixOS/nixpkgs/archive/83667ff60a88e22b76ef4b0bdf5334670b39c2b6.tar.gz"; # git-lfs 2.13
     nixpkgs-llvm_18.url = "https://github.com/NixOS/nixpkgs/archive/b5befb85475250e7849341cc2d10233415c2a528.tar.gz";
-    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/019e5d2c6611235e3f2cce7ed25156a659bbeb53.tar.gz"; # master - 2026-07-28
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/422591dcb6727393ce7f69556ccfaa11dc850543.tar.gz"; # master - 2026-08-11
     srvos.inputs.nixpkgs.follows = "nixpkgs";
-    srvos.url = "https://github.com/nix-community/srvos/archive/1ae1c0c562ef21aabdc88624bafb387fb4da7847.tar.gz";
+    srvos.url = "https://github.com/nix-community/srvos/archive/eea7edde7682348647d46e1b92ecae5c1de7da11.tar.gz";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    treefmt-nix.url = "https://github.com/numtide/treefmt-nix/archive/df3c0640565d04a0261253cdd89fce78ec50168a.tar.gz";
+    treefmt-nix.url = "https://github.com/numtide/treefmt-nix/archive/ae7910970dddc408fe6ab1c8e4b277bb21d72dc0.tar.gz";
     # keep-sorted end
   }; # End inputs
 
---
+++ hosts/nixos-beaglebone-01/configuration.nix
@@ -0,0 +1,77 @@
+{
+  inputs,
+  pkgs,
+  flake,
+  ...
+}:
+{
+  imports = [
+    inputs.srvos.nixosModules.server
+    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t420
+    flake.nixosModules.host-shared
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos-beaglebone-01";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  nix = {
+    enable = true;
+    settings.trusted-users = [ "@wheel" ];
+    settings.extra-experimental-features = [
+      "nix-command"
+      "flakes"
+      "ca-derivations"
+      "fetch-tree"
+    ];
+  };
+
+  nixpkgs.hostPlatform.system = "x86_64-linux";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable sound with pipewire.
+  security.rtkit.enable = true;
+
+  system.stateVersion = pkgs.lib.versions.majorMinor pkgs.lib.version; # initial nixos state
+
+  # on nixos this either isNormalUser or isSystemUser is required to create the user.
+  users.users.will = {
+    isNormalUser = true;
+    description = "Will Flores";
+    hashedPassword = "$6$a69Ua5IWrM6vFPtk$olkZzNeti8MosldO2.ijOSEcH713NHVeBBFk5lVoXjRj8xdu9QwLT1VFaXoU4L71JsbuMIAtcsG1PHHbD1DUb1";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "tss"
+      "docker"
+    ];
+  };
+
+  # List services that you want to enable:
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+  services.logind.settings.Login.HandleLidSwitchExternalPower = "ignore";
+  systemd.sleep.settings.Sleep = {
+    AllowSuspend = "no";
+    AllowHibernation = "no";
+    AllowHybridSleep = "no";
+    AllowSuspendThenHibernate = "no";
+  };
+}
